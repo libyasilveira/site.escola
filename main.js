d299ed04725a1d15e9e0daaf3ff69515a3b06901
libyasilveira <128653818+libyasilveira@users.noreply.github.com>
2023-09-13 07:55:37 -0300

    i

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ function TocaSomToim () {
     document.querySelector('#som_tecla_toim').play();
 }
 
-document.querySelector('.tecla-toim')
+document.querySelector('.tecla-toim').onclick = TocaSomToim;
