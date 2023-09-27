150e885ed7535561a8e31e31bc1ed69a0204a977
libyasilveira <128653818+libyasilveira@users.noreply.github.com>
2023-09-27 07:53:00 -0300

    25

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function TocaSomPom (idElementoAudio) {
+function TocaSomPom(idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
 
@@ -7,16 +7,16 @@ const Teclado = document.querySelectorAll('.tecla');
 let Contador = 0;
 
 while (Contador < Teclado.length) {
-    Teclado [Contador].onclick = function () {
-        tocar (idAudio);
+    Teclado[Contador].onclick = function () {
+        TocaSomPom(idAudio);
     }
 
     const classe = Teclado[Contador].classList[1];
-console.log(classe);
-const idAudio =`#som_${classe}`;
- console.log(idAudio);
-//'#som_{classe}'
+    console.log(classe);
+    const idAudio = `#som_${classe}`;
+    console.log(idAudio);
+    //'#som_{classe}'
 
     Contador = Contador + 1;
-    console.log (Contador);
+    console.log(Contador);
 }
