55f17bb0bf51aeb290ba5e7ea56e003d856a9479
libyasilveira <128653818+libyasilveira@users.noreply.github.com>
2023-11-14 08:12:19 -0300

    jyuh

--- main.js
+++ main.js
@@ -1,29 +1,27 @@
-function TocaSomPom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function TocaSomPom(idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
 }
 
 const Teclado = document.querySelectorAll('.tecla');
-
 let Contador = 0;
 
 while (Contador < Teclado.length) {
-    const tecla = Teclado[Contador];
-    const classe = tecla.classList[1];
-  //  console.log(classe);
-    const idAudio =`#som_${classe}`;
-  //   console.log(idAudio);
-    tecla.onclick = function () {
-        TocaSomPom(idAudio);
-    }
+  const tecla = Teclado[Contador];
+  const classe = tecla.classList[1];
+  const idAudio = `#som_${classe}`;
 
-    Contador = Contador + 1;
-   // console.log (Contador);
-   tecla.onkeydown = function (evento){
-    tecla.classList.add('ativa');
-     console.log(evento.code === 'Space' || evento.code ==='Enter');
-   }
-   tecla.onkeyup = function () {
-    tecla.classList.remove('ativa');
-   } 
+  tecla.onclick = function () {
+      TocaSomPom(idAudio);
+  }
 
+  Contador = Contador + 1;
+
+  tecla.onkeydown = function (evento) {
+      this.classList.add('ativa');
+      console.log(evento.code === 'Space' || evento.code === 'Enter');
+  }
+
+  tecla.onkeyup = function () {
+      this.classList.remove('ativa');
+  }
 }
